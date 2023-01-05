752fa281ade305d8121056f2976b60009f7f0b05
Mor Paz <mor@getport.io>
2023-01-05 13:20:46 +0200

    Add support for internal package filtering

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/y8q1v8m0
 docker build -t codefresh-package-parser ./codefresh-package-parser/
-docker tag codefresh-package-parser:latest public.ecr.aws/y8q1v8m0/codefresh-package-parser:1.0.0
+docker tag codefresh-package-parser:latest public.ecr.aws/y8q1v8m0/codefresh-package-parser:1.1.0
 docker tag codefresh-package-parser:latest public.ecr.aws/y8q1v8m0/codefresh-package-parser:latest
 docker push public.ecr.aws/y8q1v8m0/codefresh-package-parser:latest
-docker push public.ecr.aws/y8q1v8m0/codefresh-package-parser:1.0.0
+docker push public.ecr.aws/y8q1v8m0/codefresh-package-parser:1.1.0
--- codefresh-package-parser/package_parser.py
+++ codefresh-package-parser/package_parser.py
@@ -1,6 +1,9 @@
 from utils import normalize_identifier
 from port import get_port_entity, upsert_port_entity
 import xmltodict
+import logging
+
+logger = logging.getLogger('package_parser:parser')
 
 
 PACKAGE_BLUEPRINT_IDENTIFIER = 'library'
@@ -8,10 +11,15 @@ PACKAGE_RELEASE_BLUEPRINT_IDENTIFIER = 'library-release'
 
 
 def parse_packages_based_on_manager_type(port_access_token, package_manager_type, package_file_content, packages_filter):
-    if package_manager_type == 'C_SHARP_PROJECT':
+    if package_manager_type == 'C_SHARP':
+        logger.debug('Parsing C# packages')
         return parse_packages_from_c_sharp_proj(port_access_token, package_file_content, packages_filter)
     elif package_manager_type == 'NPM':
-        return {}
+        logger.debug('Parsing NPM packages')
+        return []
+    elif package_manager_type == 'JAVA':
+        logger.debug('Parsing Java packages')
+        return []
 
 
 def parse_packages_from_c_sharp_proj(port_access_token, package_file_content, packages_filter):
@@ -29,28 +37,34 @@ def parse_packages_from_c_sharp_proj(port_access_token, package_file_content, pa
 
 
 def parse_packages_from_c_sharp_item_group(port_access_token, packages_array, packages_filter: str):
+    package_language = "C#"
     packages_results = []
     for package_reference in packages_array:
+        internal_package = False
         if packages_filter.lower() in package_reference['@Include'].lower():
-            package_id = normalize_identifier(package_reference['@Include'])
-            upsert_port_entity(port_access_token, PACKAGE_BLUEPRINT_IDENTIFIER, {
-                'identifier': package_id,
-                'title': package_id,
-                "properties": {}
-            })
-            package_release_id = f'{package_id}_{normalize_identifier(package_reference["@Version"])}'
-            upsert_port_entity(port_access_token, PACKAGE_RELEASE_BLUEPRINT_IDENTIFIER, {
-                'identifier': package_release_id,
-                'title': package_release_id,
-                "properties": {
-                    "Version": package_reference["@Version"]
-                },
-                "relations": {
-                    "library": package_id
-                }
+            internal_package = True
+        package_id = normalize_identifier(package_reference['@Include'])
+        upsert_port_entity(port_access_token, PACKAGE_BLUEPRINT_IDENTIFIER, {
+            'identifier': package_id,
+            'title': package_id,
+            "properties": {
+                "Language": package_language,
+                "Internal": internal_package
+            }
+        })
+        package_release_id = f'{package_id}_{normalize_identifier(package_reference["@Version"])}'
+        upsert_port_entity(port_access_token, PACKAGE_RELEASE_BLUEPRINT_IDENTIFIER, {
+            'identifier': package_release_id,
+            'title': package_release_id,
+            "properties": {
+                "Version": package_reference["@Version"]
+            },
+            "relations": {
+                "library": package_id
+            }
 
-            })
-            packages_results.append(package_release_id)
+        })
+        packages_results.append(package_release_id)
     return packages_results
 
 
